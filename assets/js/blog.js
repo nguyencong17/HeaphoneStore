390e2c31791514c7f94fa996c49ba8737e4adace
duycong <cnd.congnguyenduy@gmail.com>
2021-09-13 10:36:39 +0700

    fix call api load data product

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,11 +1,10 @@
 /**Load data product */
 
-let courseApi = 'http://localhost:3000/posts';
+let courseApi = 'https://api-headphone-store.herokuapp.com/api/posts';
 fetch(courseApi)
     .then(response => {return response.json()})
     .then(function(course){
         let htmls = course.map(function(item){  
-            console.log(item.image);
             return `
             <div class="blog__view">
             <div class="blog__view--image">
--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,11 +1,10 @@
 /**Load data product */
 
-let courseApi = 'http://localhost:3000/products';
+let courseApi = 'https://api-headphone-store.herokuapp.com/api/products';
 fetch(courseApi)
     .then(response => {return response.json()})
     .then(function(course){
         let htmls = course.map(function(item){  
-            console.log(item.image);
             return `
             <div class="item">
             <div class="around">
@@ -29,7 +28,7 @@ fetch(courseApi)
             <a href="detail.html">
               <h4>${item.name}</h4>
             </a>
-            <p>${item.price}</p>
+            <p>${item.price}.000 đ</p>
           </div>
             `
         })
@@ -37,34 +36,3 @@ fetch(courseApi)
         let html = htmls.join('');
         document.getElementById('data-product').innerHTML = html;
     })
-
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
-
-// Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
-  }
-  // Continue to JSON Server router
-  next()
-})
-
-// Use default router
-const PORT = process.env.PORT || 3000;
-server.use(router)
-server.listen(PORT, () => {
-  console.log('JSON Server is running')
-})
