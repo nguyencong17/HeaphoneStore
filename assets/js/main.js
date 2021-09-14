da1044f2e6d7229c9d2fe247a0b7a158a74294ab
duycong <cnd.congnguyenduy@gmail.com>
2021-09-14 15:16:50 +0700

    fix prev next slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,34 @@
 /* slider */
 let index = 1;
+let images = ['assets/image/bg.png','assets/image/bg2.png','assets/image/bg3.png'];
 changeImage = function(){
-    let images = ['assets/image/bg.png','assets/image/bg2.png','assets/image/bg3.png'];
-    
     document.getElementById('background').src = images[index];
     index++;
     if(index==3){
         index=0;
     }
 }
-setInterval(changeImage,3000);
+setInterval(changeImage,7000);
+
+// event click change slider
+
+const prevSlide = () => {
+    document.getElementById('background').src = images[index];
+    index--;
+    if(index==-1){
+        index=2;
+    }
+}
+const nextSlide = () => {
+    document.getElementById('background').src = images[index];
+    index++;
+    if(index==3){
+        index=0;
+    }
+}
+
+document.getElementById('prev').addEventListener('click',prevSlide);
+document.getElementById('next').addEventListener('click',nextSlide);
 
 
 /* video */
